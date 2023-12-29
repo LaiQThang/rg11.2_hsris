12c144d8519b033f3b40e6ec9f0149ad08c8d4b7
LongPV772003 <phamvanlong7720033@gmail.com>
2023-12-29 23:48:05 +0700

    update layout and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import DefaultLayout from '~/layouts/DefaultLayout/DefaultLayout';
+import { Fragment } from 'react';
 
 function App() {
-  return (
-   
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Routes>
+					{publicRoutes.map((route, index) => {
+						let Layout = DefaultLayout;
+						if (route.layout) {
+							Layout = route.layout;
+						} else if (route.layout === null) {
+							Layout = Fragment;
+						}
+						const Page = route.component;
+						return (
+							<Route
+								key={index}
+								path={route.path}
+								element={
+									<Layout>
+										<Page />
+									</Layout>
+								}
+							/>
+						);
+					})}
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ client/src/routes/index.js
@@ -0,0 +1,30 @@
+import config from '~/config';
+
+import Home from '~/pages/headerPages/Home';
+import Notification from '~/pages/headerPages/Notification';
+import Work from '~/pages/headerPages/Work';
+import Achievement from '~/pages/sidebarPages/Achievement/Achievement';
+import HistoryRegisterResearch from '~/pages/sidebarPages/HistoryRegisterResearch/HistoryRegisterResearch';
+import HistoryRegisterTopic from '~/pages/sidebarPages/HistoryRegisterTopic/HistoryRegisterTopic';
+import ProFile from '~/pages/sidebarPages/ProFile/ProFile';
+import ProgressTopic from '~/pages/sidebarPages/ProgressTopic/ProgressTopic';
+import RegisterResearch from '~/pages/sidebarPages/RegisterResearch/RegisterResearch';
+import RegisterTopic from '~/pages/sidebarPages/RegisterTopic/RegisterTopic';
+import Login from '~/pages/Login/login';
+const publicRoutes = [
+	{ path: config.routes.home, component: Home },
+	{ path: config.routes.notification, component: Notification },
+	{ path: config.routes.work, component: Work },
+	{ path: config.routes.achievement, component: Achievement },
+	{ path: config.routes.historyRegisterResearch, component: HistoryRegisterResearch },
+	{ path: config.routes.historyRegisterTopic, component: HistoryRegisterTopic },
+	{ path: config.routes.profile, component: ProFile },
+	{ path: config.routes.progressTopic, component: ProgressTopic },
+	{ path: config.routes.registerResearch, component: RegisterResearch },
+	{ path: config.routes.registerTopic, component: RegisterTopic },
+	{ path: config.routes.login, component: Login, layout: null},
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
