616129e634475d0394e788fa21fec932336680c3
Lại Quang Thắng <74010343+LaiQThang@users.noreply.github.com>
2024-01-10 16:38:18 +0700

    Update index.js

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,7 +1,6 @@
 import config from '~/config';
 
-import Logout from '~/pages/Logout';
-import Home from '~/pages/headerPages/Home';
+import Home from '~/pages/headerPages/Home/homePage';
 import Notification from '~/pages/headerPages/Notification';
 import Work from '~/pages/headerPages/Work';
 import Achievement from '~/pages/sidebarPages/Achievement/Achievement';
@@ -10,14 +9,9 @@ import HistoryRegisterTopic from '~/pages/sidebarPages/HistoryRegisterTopic/Hist
 import ProFile from '~/pages/sidebarPages/ProFile/ProFile';
 import ProgressTopic from '~/pages/sidebarPages/ProgressTopic/ProgressTopic';
 import RegisterResearch from '~/pages/sidebarPages/RegisterResearch/RegisterResearch';
+import DetailResearch from '~/pages/sidebarPages/DetailResearch/DetailResearch';
 import RegisterTopic from '~/pages/sidebarPages/RegisterTopic/RegisterTopic';
 import Login from '~/pages/Login/login';
-
-import AddTopic from '~/pages/sidebarPages/AddTopic/AddTopic';
-import TrackProgress from '~/pages/sidebarPages/TrackProgress/TrackProgress';
-import TrackProgressDetail from '~/pages/sidebarPages/TrackProgress/TrackProgressDetail';
-import SetupProgress from '~/pages/sidebarPages/SetupProgress/SetupProgress';
-import TopicGroup from '~/pages/sidebarPages/TopicGroup/TopicGroup';
 const publicRoutes = [
 	{ path: config.routes.home, component: Home },
 	{ path: config.routes.notification, component: Notification },
@@ -29,14 +23,8 @@ const publicRoutes = [
 	{ path: config.routes.progressTopic, component: ProgressTopic },
 	{ path: config.routes.registerResearch, component: RegisterResearch },
 	{ path: config.routes.registerTopic, component: RegisterTopic },
+	{ path: config.routes.detailResearch, component: DetailResearch},
 	{ path: config.routes.login, component: Login, layout: null},
-	{ path: config.routes.logout, component: Logout },
-
-	{ path: config.routes.addTopic, component: AddTopic },
-	{ path: config.routes.trackProgress, component: TrackProgress },
-	{ path: config.routes.setupProgress, component: SetupProgress },
-	{ path: config.routes.topicGroup, component: TopicGroup },
-	{ path: config.routes.trackProgressDetail, component: TrackProgressDetail },
 ];
 
 const privateRoutes = [];
