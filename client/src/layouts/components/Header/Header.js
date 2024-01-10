16ff44b5905493eb0d8eb4b9b25f9ea9d00283b0
Lại Quang Thắng <74010343+LaiQThang@users.noreply.github.com>
2024-01-10 16:15:30 +0700

    Update Header.js

--- client/src/layouts/components/Header/Header.js
+++ client/src/layouts/components/Header/Header.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+//import Offcanvas from 'react-bootstrap/Offcanvas';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Menu from '~/Components/Menu';
@@ -7,37 +8,43 @@ import { MenuItem } from '~/Components/Menu';
 import config from '~/config';
 import { faBell, faFile, faHome, faPen } from '@fortawesome/free-solid-svg-icons';
 import Button from '~/Components/Button';
-import { useState } from 'react';
 const cx = classNames.bind(styles);
 
+function toggleSidebar() {
+	var sidebar = document.getElementById('sidebar');
+	sidebar.classList.toggle('open');
+}
+
 function Header() {
-	const [isClickedEN, setIsClickedEN] = useState(false);
-	const [isClickedVI, setIsClickedVI] = useState(true);
-	const handleActiveEN =()=>{
-		setIsClickedVI(false);
-		setIsClickedEN(true);
-	}
-	const handleActiveVI =()=>{
-		setIsClickedVI(true);
-		setIsClickedEN(false);
-	}
 	return (
 		<div className={cx('wrapper')}>
 			<div className={cx('inner')}>
 				<div className={cx('language')}>
-					<div onClick = {handleActiveEN} className={cx(isClickedEN ? 'text-active' : 'text')}>EN</div>
-					<span className={cx('text-line')}>|</span>
-					<div onClick = {handleActiveVI} className={cx(isClickedVI ? 'text-active' : 'text')}>Tiếng Việt</div>
+					<div className={cx('text')}>ENGLISH</div>
+					<span className={cx('text')}>|</span>
+					<div className={cx('text')}>Tiếng Việt</div>
 				</div>
 				<Menu className={cx('nav-list')}>
-					<MenuItem isSideBar = {false} title={'Trang Chủ'} to={config.routes.home} icon={<FontAwesomeIcon icon={faHome} />}/>
+					<MenuItem title={'Trang Chủ'} to={config.routes.home} icon={<FontAwesomeIcon icon={faHome} />} />
 					<MenuItem
-					isSideBar = {false}
+						className={cx('nav-item')}
+						title={'Trang Chủ'}
+						to={config.routes.home}
+						icon={<FontAwesomeIcon icon={faHome} />}
+					/>
+					<MenuItem
+						className={cx('nav-item')}
+						title={'Hoạt Động'}
+						to={config.routes.work}
+						icon={<FontAwesomeIcon icon={faFile} />}
+					/>
+					<MenuItem
+						className={cx('nav-item')}
 						title={'Thông Báo'}
 						to={config.routes.notification}
 						icon={<FontAwesomeIcon icon={faBell} />}
 					/>
-					<MenuItem isSideBar = {false} title={'Hoạt Động'} to={config.routes.work} icon={<FontAwesomeIcon icon={faFile} />}/>
+					<MenuItem title={'Hoạt Động'} to={config.routes.work} icon={<FontAwesomeIcon icon={faFile} />} />
 					<Button text leftIcon={<FontAwesomeIcon icon={faPen} />}>
 						Đăng Ký Đề Tài
 					</Button>
