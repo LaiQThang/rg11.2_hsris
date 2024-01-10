9d617f305890ebc03f581fdb00cb40ebeeef7892
Lại Quang Thắng <74010343+LaiQThang@users.noreply.github.com>
2024-01-10 16:14:08 +0700

    Update Header.js

--- client/src/layouts/components/Header/Header.js
+++ client/src/layouts/components/Header/Header.js
@@ -1,6 +1,5 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-//import Offcanvas from 'react-bootstrap/Offcanvas';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Menu from '~/Components/Menu';
@@ -10,36 +9,13 @@ import { faBell, faFile, faHome, faPen } from '@fortawesome/free-solid-svg-icons
 import Button from '~/Components/Button';
 const cx = classNames.bind(styles);
 
-function toggleSidebar() {
-	var sidebar = document.getElementById('sidebar');
-	sidebar.classList.toggle('open');
-}
-
 function Header() {
 	return (
 		<div className={cx('wrapper')}>
-			<div className={cx('inner')}>
-				<div className={cx('language')}>
-					<div className={cx('text')}>ENGLISH</div>
-					<span className={cx('text')}>|</span>
-					<div className={cx('text')}>Tiếng Việt</div>
-				</div>
+	@@ -21,6 +27,19 @@ function Header() {
 				<Menu className={cx('nav-list')}>
 					<MenuItem title={'Trang Chủ'} to={config.routes.home} icon={<FontAwesomeIcon icon={faHome} />} />
 					<MenuItem
-						className={cx('nav-item')}
-						title={'Trang Chủ'}
-						to={config.routes.home}
-						icon={<FontAwesomeIcon icon={faHome} />}
-					/>
-					<MenuItem
-						className={cx('nav-item')}
-						title={'Hoạt Động'}
-						to={config.routes.work}
-						icon={<FontAwesomeIcon icon={faFile} />}
-					/>
-					<MenuItem
-						className={cx('nav-item')}
 						title={'Thông Báo'}
 						to={config.routes.notification}
 						icon={<FontAwesomeIcon icon={faBell} />}
