4fb6391fa9b1cc7f70aff5857403dc094c3f384c
Lại Quang Thắng <74010343+LaiQThang@users.noreply.github.com>
2024-01-10 15:45:09 +0700

    Update MenuItem.js

--- client/src/Components/Menu/MenuItem.js
+++ client/src/Components/Menu/MenuItem.js
@@ -1,22 +1,13 @@
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import styles from './Menu.module.scss';
-import { useState } from 'react';
+
 const cx = classNames.bind(styles);
 
 function MenuItem({ title, to, icon }) {
-	const [isActive, setIsActive] = useState(false);
-
-	// Hàm xử lý sự kiện onClick
-	const handleClick = () => {
-		setIsActive(true);
-	};
 	return (
-		<NavLink to={to} className={cx('menu-item')} onClick={handleClick}>
+		<NavLink to={to} className={cx('menu-item')}>
 			<div className={cx('icon')}>{icon}</div>
 			<span className={cx('title')}>{title}</span>
 		</NavLink>
-	);
-}
-
-export default MenuItem;
+	
