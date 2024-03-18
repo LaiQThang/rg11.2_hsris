127a7b3d9cb9020a95056dca9b64040727f58d96
Lại Quang Thắng <74010343+LaiQThang@users.noreply.github.com>
2024-03-18 22:03:36 +0700

    fix conflict

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -19,32 +19,6 @@ import TrackProgressDetail from '~/pages/sidebarPages/TrackProgress/TrackProgres
 import SetupProgress from '~/pages/sidebarPages/SetupProgress/SetupProgress';
 import TopicGroup from '~/pages/sidebarPages/TopicGroup/TopicGroup';
 import Logout from '~/pages/Logout';
-import reviewTopic from '~/pages/sidebarPages/reviewTopic/reviewTopic';
-import RequireAuth from '~/Components/Auth/RequiedAuth';
-import addResearch from '~/pages/sidebarPages/addResearch/addResearch';
-const publicRoutes = [
-	{ path: config.routes.home, component: Home, auth: RequireAuth },
-	{ path: config.routes.notification, component: Notification, auth: RequireAuth },
-	{ path: config.routes.work, component: Work, auth: RequireAuth },
-	{ path: config.routes.achievement, component: Achievement, auth: RequireAuth },
-	{ path: config.routes.historyRegisterResearch, component: HistoryRegisterResearch, auth: RequireAuth },
-	{ path: config.routes.historyRegisterTopic, component: HistoryRegisterTopic, auth: RequireAuth },
-	{ path: config.routes.profile, component: ProFile, auth: RequireAuth },
-	{ path: config.routes.progressTopic, component: ProgressTopic, auth: RequireAuth },
-	{ path: config.routes.registerResearch, component: RegisterResearch, auth: RequireAuth },
-	{ path: config.routes.registerTopic, component: RegisterTopic, auth: RequireAuth },
-	{ path: config.routes.detailResearch, component: DetailResearch, auth: RequireAuth},
-	{ path: config.routes.detailTopic, component: DetailTopic, auth: RequireAuth},
-	{ path: config.routes.login, component: Login, layout: null},
-	// { path: config.routes.login, component: Login, layout: null },
-	{ path: config.routes.logout, component: Logout, auth: RequireAuth },
-	{ path: config.routes.addTopic, component: AddTopic, auth: RequireAuth },
-	{ path: config.routes.trackProgress, component: TrackProgress, auth: RequireAuth },
-	{ path: config.routes.setupProgress, component: SetupProgress, auth: RequireAuth },
-	{ path: config.routes.topicGroup, component: TopicGroup, auth: RequireAuth },
-	{ path: config.routes.trackProgressDetail, component: TrackProgressDetail, auth: RequireAuth },
-	{ path: config.routes.reviewTopic, component: reviewTopic, auth: RequireAuth },
-	{ path: config.routes.addResearch, component: addResearch, auth: RequireAuth },
 ];
 
 const privateRoutes = [];
