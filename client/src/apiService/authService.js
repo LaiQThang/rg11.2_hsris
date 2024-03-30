d413a60ee1c86521255ede63efc21b8a5388fdf8
Admin <=>
2024-03-30 11:04:24 +0700

    update authService

--- client/src/apiService/authService.js
+++ client/src/apiService/authService.js
@@ -71,14 +71,14 @@ export const detailTopic = async (tokenBearer,id) => {
         console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
     }
 };
-export const permission = async (id) => {
+export const permission = async (id,tokenBearer) => {
     try {
         const config= {
 
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
-                // 'Authorization': `Bearer ${tokenBearer}`
+                'Authorization': `Bearer ${tokenBearer}`
             },
         }
         const res = await request.get(`/v1/permission/${id}`,config);
@@ -104,6 +104,59 @@ export const updateResearch = async (data) => {
         console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
     }
 };
+export const registerTopicConfirm = async (id,tokenBearer) => {
+    try {
+        const data ={
+            idDT: id
+        }
+        const header = {
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${tokenBearer}`
+            }
+        }
+        const res = await request.post('/v1/ct-detai', data, header);
+        return res;
+    } catch (error) {
+        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
+    }
+};
+export const updateNewTopic = async(data,tokenBearer) =>{
+    try {
+        
+        const header = {
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${tokenBearer}`
+            }
+        }
+        const res = await request.post('/v1/detai', data, header);
+        return res;
+    } catch (error) {
+        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
+    }
+}
+export const AddTopic = async(data,tokenBearer) =>{
+    try {
+        
+        const header = {
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${tokenBearer}`
+            }
+        }
+        const res = await request.post('/v1/detai-giangvien', data, header);
+        return res;
+    } catch (error) {
+        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
+    }
+}
 export const getResearch = async(year)=>{
     try{
         const header = {
@@ -118,6 +171,26 @@ export const getResearch = async(year)=>{
             }
         }
         const res = await request.get('/v1/huongnghiencuu',header)
+        return res.data
+    }
+    catch(e){
+        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
+    }
+}
+export const getTopicTeacher = async(year,tokenBearer)=>{
+    try{
+        const header = {
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${tokenBearer}`
+            },
+            params:{
+                year: year,
+            }
+        }
+        const res = await request.get('/v1/detai-giangvien-xetduyet',header)
         return res.data
     }
     catch(e){
@@ -144,28 +217,145 @@ export const registerTopic = async (tokenBearer,year) => {
         console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
     }
 };
-
-export const postDetailReseach = async (idHNC, idSV) => {
+export const browseTopic = async (tokenBearer,id) => {
     try {
-        const array = {
-            idHNC: idHNC,
-            idSV: idSV,
-        }
         const config= {
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            //'Authorization': `Bearer ${tokenBearer}`
-        }
+            'Authorization': `Bearer ${tokenBearer}`
+        },
     }
-    const res = await request.post('/v1/hncsinhvien', array,config);
-    return res;
+    const data = {
+        idDT: id
+    }
+    const res = await request.post('/v1/detai-giangvien-xetduyet-post',data,config);
+    return res.data;
     }
     catch (error)
     {
         console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
     }
 };
+export const browseTopicAdmin = async (id) => {
+    try {
+        const config= {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            // 'Authorization': `Bearer ${tokenBearer}`
+        },
+    }
+    const data = {
+        idDT: id
+    }
+    const res = await request.post('/v1/xet-duyet/update',data,config);
+    return res.data;
+    }
+    catch (error)
+    {
+        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
+    }
+};
+export const reportStudent = async (formData,tokenBearer) => {
+    try {
+        const config= {
+        headers: {
+            'content-type': 'multipart/form-data',
+            'Authorization': `Bearer ${tokenBearer}`
+        },
+    }
+    const res = await request.post('/v1/ct-baocaosinhvien',formData,config);
+    return res.data;
+    }
+    catch (error)
+    {
+        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
+    }
+};
+export const reportTeacher = async (data,tokenBearer) => {
+    try {
+        const config= {
+        headers: {
+            'content-type': 'multipart/form-data',
+            'Authorization': `Bearer ${tokenBearer}`
+        },
+    }
+    const res = await request.post('/v1/nhanxet',data,config);
+    return res.data;
+    }
+    catch (error)
+    {
+        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
+    }
+};
+export const createScoreCard = async (data,tokenBearer) => {
+    try {
+        const config= {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${tokenBearer}`
+        },
+    }
+    const res = await request.post('/v1/hoidong/point',data,config);
+    return res.data;
+    }
+    catch (error)
+    {
+        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
+    }
+};
+export const updateScore = async (data,tokenBearer) => {
+    try {
+        const config= {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${tokenBearer}`
+        },
+    }
+    const res = await request.post('v1/giaithuong/capnhatdiem',data,config);
+    return res.data;
+    }
+    catch (error)
+    {
+        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
+    }
+};
+export const updateAward = async (data,tokenBearer) => {
+    try {
+        const config= {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${tokenBearer}`
+        },
+    }
+    const res = await request.post('v1/giaithuong',data,config);
+    return res.data;
+    }
+    catch (error)
+    {
+        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản:', error);
+    }
+};
+export const getNameTeacher = async()=>{
+    try{
+        const config ={
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }
+        const res = await request.get('/v1/giangvien',config)
+        return res.data
+    }
+    catch(e){
+        console.log('Đã xãy ra lỗi khi lấy dữ liệu',e)
+    }
+}
+// thang
 export const getAllHNC = async(year, tokenBearer)=>{
     try{
         const header = {
@@ -187,33 +377,45 @@ export const getAllHNC = async(year, tokenBearer)=>{
     }
 }
 
-export const postTopicGroup = async(name, note, contents, listStudents,HNC, tokenBearer )=>{
+export const postTopicGroup = async(data, tokenBearer )=>{
     try{
-        const array = {
-            name: name,
-            note: note,
-            contents: contents,
-            students: listStudents,
-            HNC: HNC,
-
-        }
         const header = {
 
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
-                 'Authorization':` Bearer ${tokenBearer}`
+                 'Authorization': `Bearer ${tokenBearer}`
             },
         }
-        const res = await request.post('/v1/bienbanphancong', array,header)
+        const res = await request.get('/v1/bienbanphancong', data,header)
         return res.data
     }
     catch(e){
         console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
     }
 }
+export const getTopicAdmin = async(year,id)=>{
+    try{
+        const header = {
 
-export const getSetupProgress = async(year, tokenBearer)=>{
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                //  'Authorization': `Bearer ${tokenBearer}`
+            },
+            params:{
+                y:year,
+                i:id
+            }
+        }
+        const res = await request.get('/v1/xet-duyet/list', header)
+        return res.data
+    }
+    catch(e){
+        console.log('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
+    }
+}
+export const getProgressTopic = async(year,tokenBearer)=>{
     try{
         const header = {
 
@@ -223,103 +425,90 @@ export const getSetupProgress = async(year, tokenBearer)=>{
                 'Authorization': `Bearer ${tokenBearer}`
             },
             params:{
-                y: year,
+                year:year,
             }
         }
-        const res = await request.get('/v1/baocaodetai-listnhom',header)
+        const res = await request.get('/v1/baocaodetai', header)
         return res.data
     }
     catch(e){
-        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
+        console.log('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
     }
 }
-
-export const postSetupProgress = async(idDT, idBB,timeArray, tokenBearer )=>{
-    try{
-        const array = {
-            idDT: idDT,
-            idBB: idBB,
-            timeArray: timeArray
-        }
-        const header = {
-
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                 'Authorization':` Bearer ${tokenBearer}`
-            },
-        }
-        const res = await request.post('/v1/baocaodetai', array,header)
-        return res.data
-    }
-    catch(e){
-        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
-    }
-}
-
-export const getAllTeacher = async()=>{
+export const getTrackProgress = async(year,tokenBearer)=>{
     try{
         const header = {
 
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
-                //'Authorization': `Bearer ${tokenBearer}`
-            },
-
-        }
-        const res = await request.get('/v1/giangvien',header)
-        return res.data
-    }
-    catch(e){
-        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
-    }
-}
-
-export const getTopicNoCouncil = async(year)=>{
-    try{
-        const header = {
-
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                //'Authorization': `Bearer ${tokenBearer}`
+                'Authorization': `Bearer ${tokenBearer}`
             },
             params:{
-                y: year,
+                y:year,
             }
-
         }
-        const res = await request.get('/v1/hoidong/list-no-council',header)
+        const res = await request.get('/v1/nhanxet/view', header)
         return res.data
     }
     catch(e){
-        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
+        console.log('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
     }
 }
-
-export const postAddCouoncil = async(tenHD, ngayCham,diaDiem, ghiChu,topicArr, teacherArr, tokenBearer )=>{
+export const getViewPoint = async(year,tokenBearer)=>{
     try{
-        const array = {
-            tenHD: tenHD,
-            ngayCham: ngayCham,
-            diaDiem: diaDiem,
-            ghiChu: ghiChu,
-            topicArr: topicArr,
-            teacherArr: teacherArr,
-        }
         const header = {
 
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
-                 'Authorization':` Bearer ${tokenBearer}`
+                'Authorization': `Bearer ${tokenBearer}`
             },
+            params:{
+                y:year,
+            }
         }
-        const res = await request.post('/v1/hoidong', array,header)
+        const res = await request.get('/v1/hoidong/view-point', header)
         return res.data
     }
     catch(e){
-        console.error('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
+        console.log('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
+    }
+}
+export const getPointAdmin = async(year,tokenBearer)=>{
+    try{
+        const header = {
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${tokenBearer}`
+            },
+            params:{
+                y:year,
+            }
+        }
+        const res = await request.get('/v1/giaithuong/tinhdiem', header)
+        return res.data
+    }
+    catch(e){
+        console.log('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
+    }
+}
+export const getAward = async(tokenBearer)=>{
+    try{
+        const header = {
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${tokenBearer}`
+            },
+        }
+        const res = await request.get('/v1/giaithuong/list', header)
+        return res.data
+    }
+    catch(e){
+        console.log('Đã xảy ra lỗi khi lấy dữ liệu tài khoản', e)
     }
 }
