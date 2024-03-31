d9020bf67521b7c5aab6bd8659ce628e417f50f8
Lại Quang Thắng <74010343+LaiQThang@users.noreply.github.com>
2024-03-31 22:29:31 +0700

    fix: update env

--- client/src/utils/requests.js
+++ client/src/utils/requests.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
-    // baseURL: 'process.env.REACT_APP_BASE_URL',
+    // baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'process.env.REACT_APP_BASE_URL',
     
 });
 
