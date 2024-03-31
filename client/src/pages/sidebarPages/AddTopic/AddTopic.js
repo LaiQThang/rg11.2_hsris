13625220cef80484a371b5e8ca1bfd58bd361343
Lại Quang Thắng <74010343+LaiQThang@users.noreply.github.com>
2024-03-31 18:53:31 +0700

    fix: fix conflict

--- client/src/pages/sidebarPages/AddTopic/AddTopic.js
+++ client/src/pages/sidebarPages/AddTopic/AddTopic.js
@@ -13,12 +13,38 @@ const cx = classNames.bind(styles);
 function AddTopic() {
 	const [dataHNC,setDataHNC] = useState([])
 	const {register,reset,handleSubmit} = useForm()
+	const [showNameNotify, setShowNameNotify] = useState(true);
+	const [showSummaryNotify, setShowSummaryNotify] = useState(true);
+	const [showTargetNotify, setShowTargetNotify] = useState(true);
+	const [showLimitNotify, setShowLimitNotify] = useState(true);
+	
 	const[id,setId] = useState('')
 	const auth = useAuth()
 	const tokenBearer = auth.getTokens()
-	const d = new Date();
-	const year = d.getFullYear();
+	const year ='2024'
+	
+	const handleChangInput = (e) => {
+		const inputValue = e.target.value;
+		// Kiểm tra từng trường và cập nhật hiển thị thông báo tương ứng
+		switch (e.target.name) {
+			case 'name':
+				setShowNameNotify(inputValue.trim() === '');
+				break;
+			case 'summary':
+				setShowSummaryNotify(inputValue.trim() === '');
+				break;
+			case 'target':
+				setShowTargetNotify(inputValue.trim() === '');
+				break;
+			case 'limit':
+				setShowLimitNotify(inputValue.trim() === '');
+				break;
+			default:
+				break;
+		}
+	};
 
+	
 	useEffect(()=>{
 		fetchApi().then((res)=>{
 			setDataHNC(res.data)
@@ -33,19 +59,26 @@ function AddTopic() {
 	}
 	const handleAddTopic = async(data)=>{
 		console.log(data)
-		let result = Result.AddTopic(data,tokenBearer.access_token)
-		console.log(result);
-		if(result){
-			showToast('success','Thêm đề tài thành công rực rỡ ')
+		if(showNameNotify || showSummaryNotify || showTargetNotify || showLimitNotify){
+			showToast('error','Vui lòng nhập đầy đủ trường dữ liệu')
 		}
 		else{
-			showToast('error','Thêm đề tài thất bại thảm hại ')
+			let result = Result.AddTopic(data,tokenBearer.access_token)
+			if(result){
+				showToast('success','Thêm đề tài thành công')
+				reset()
+				}
+				else {
+					showToast('error','Thêm đề tài thất bại')
+				}
+			}	
 		}
-	}
+
 	console.log(id)
 	return (
 		<div className={cx('wrapper')}>
 			<ToastContainer/>
+			<form className={cx('table')} onSubmit={handleSubmit(handleAddTopic)}>
 			<div className={cx('inner')}>
 				<div className={cx('name-page')}>Quản lý chung - Thêm đề tài</div>
 				<div className={cx('frame-container')}>
@@ -78,45 +111,50 @@ function AddTopic() {
 									</div>
 								</div>
 								<div className={cx('content-item-card')}>
-									<div className={cx('item')}>
-										<div className={cx('item-title')}>Tên đề tài</div>
-										<div className={cx('item-content')}>
-											<textarea type="text" className={cx('input')} placeholder="Tên đề tài" {...register('name')}/>
-										</div>
+								<div className={cx('item')}>
+									<div className={cx('item-title')}>Tên đề tài</div>
+									{showNameNotify && <div className={cx('notify')}>Tên nhóm không được để trống</div>}
+									<div className={cx('item-content')}>
+										<textarea  type="text" className={cx('input')} placeholder="Tên đề tài" {...register('name')} onChange={handleChangInput}/>
 									</div>
+								</div>
 								</div>
 							</div>
 
 							<div className={cx('item')}>
 								<div className={cx('item-title')}>Tóm tắt</div>
+								{showSummaryNotify && <div className={cx('notify')}>Tên nhóm không được để trống</div>}
 								<div className={cx('item-content')}>
-									<textarea type="text" className={cx('input')} placeholder="Tóm tắt" {...register('summary')}/>
+									<textarea type="text" className={cx('input')} placeholder="Tóm tắt" {...register('summary')} onChange={handleChangInput}/>
 								</div>
 							</div>
 
 							<div className={cx('item')}>
 								<div className={cx('item-title')}>Mục tiêu</div>
+								{showTargetNotify && <div className={cx('notify')}>Tên nhóm không được để trống</div>}
 								<div className={cx('item-content')}>
-									<textarea type="text" className={cx('input')} placeholder="Mục tiêu" {...register('target')}/>
+									<textarea  type="text" className={cx('input')} placeholder="Mục tiêu" {...register('target')} onChange={handleChangInput}/>
 								</div>
 							</div>
 
 							<div className={cx('item')}>
 								<div className={cx('item-title')}>Phạm vi</div>
+								{showLimitNotify && <div className={cx('notify')}>Tên nhóm không được để trống</div>}
 								<div className={cx('item-content')}>
-									<textarea type="text" className={cx('input')} placeholder="Phạm vi" {...register('limit')}/>
+									<textarea  type="text" className={cx('input')} placeholder="Phạm vi" {...register('limit')} onChange={handleChangInput}/>
 								</div>
 							</div>
 
 							<div className={cx('btn-container')}>
-								<div className={cx('btn-save')} onClick={handleSubmit(handleAddTopic)}>
+								<button className={cx('btn-save')}>
 									Lưu
-								</div>
+								</button>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+			</form>
 		</div>
 	);
 }
--- client/src/pages/sidebarPages/TopicGroup/TopicGroup.js
+++ client/src/pages/sidebarPages/TopicGroup/TopicGroup.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from './TopicGroup.module.scss';
 import classNames from 'classnames/bind';
-import * as Result from '~/apiService/authService';
+import * as Result from '~/apiService/authService'
 import { useParams } from 'react-router-dom';
 import { useAuth } from '~/Components/Auth';
 import { showToast } from '~/Components/ToastMessage/Toast';
@@ -11,82 +11,86 @@ import { useForm } from 'react-hook-form';
 const cx = classNames.bind(styles);
 
 function TopicGroup() {
-	const auth = useAuth();
-	const tokenBearer = auth.getTokens();
+    const auth = useAuth()
+	const tokenBearer = auth.getTokens()
 	const groupSelectRef = useRef(null);
-	const { register, handleSubmit } = useForm();
+	const {register, handleSubmit} = useForm();
 	const [selectedYear, setSelectedYear] = useState('2024');
-	const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 	const [groupName, setGroupName] = useState('');
 	const [showNotify, setShowNotify] = useState(true);
 
-	const [idStudent, setIdStudent] = useState('');
-	const [arrStudents, setArrStudents] = useState([]);
+	const [idStudent, setIdStudent] = useState('')
+	const [arrStudents, setArrStudents] = useState([])
 	const [idHNC, setIdHNC] = useState('');
-	const filterHNC = data.filter((newData) => newData.id === idHNC);
-
+	const filterHNC = data.filter(newData => newData.id===idHNC)
+	
 	const handleGroupNameChange = (event) => {
-		const value = event.target.value;
-		setGroupName(value);
-		setShowNotify(value.trim() === '');
-	};
+        const value = event.target.value;
+        setGroupName(value);
+        setShowNotify(value.trim() === '');
+    };
 
 	const handleYearChange = (event) => {
-		const selectedYearValue = event.target.value;
-		setSelectedYear(selectedYearValue);
-	};
-
+        const selectedYearValue = event.target.value;
+        setSelectedYear(selectedYearValue);
+    };
+	
 	//thay doi lua chon HNC
-	const handleChangeSelected = (e) => {
-		const initialGroupId = e.target.value;
-		setIdHNC(initialGroupId);
-		setArrStudents([]);
-
+	const handleChangeSelected = e => {
+        const initialGroupId = e.target.value;
+        setIdHNC(initialGroupId);
+		setArrStudents([])
+		
 		console.log(idHNC);
-	};
-
+	}
+	
 	//them sinh vien
 	const handleAddStudent = (id) => {
 		setIdStudent(id);
 		if (arrStudents.length === 0) {
 			setArrStudents([id]);
 		} else {
-			setArrStudents((prevArrStudents) => [...prevArrStudents, id]);
+			setArrStudents(prevArrStudents => [...prevArrStudents, id]);
 		}
+		
 	};
 	const handleDeleteStudent = (id) => {
-		const updatedStudents = arrStudents.filter((idStudent) => id !== idStudent);
+		const updatedStudents = arrStudents.filter(idStudent => id !== idStudent);
 		setArrStudents(updatedStudents);
-	};
-	useEffect(() => {
-		fetchApi().then((data) => {
-			setData(data.data);
-		});
-		console.log(filterHNC);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-	const fetchApi = async () => {
-		let result;
-		result = await Result.getAllHNC(selectedYear, tokenBearer.access_token);
-		return result;
-	};
+	}
+	useEffect(()=>{
+		fetchApi().then((data)=>{
+			setData(data.data)
+		})
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	},[selectedYear])
+	const fetchApi = async ()=>{
+		let result
+		result = await Result.getAllHNC(selectedYear, tokenBearer.access_token)
+		return result
+	}
 
 	const posthApi = async () => {
 		try {
-			const result = await Result.postTopicGroup(groupName, arrStudents, idHNC, tokenBearer.access_token);
+			const result = await Result.postTopicGroup(groupName,'' ,'',arrStudents,idHNC, tokenBearer.access_token);
 			return result;
 		} catch (error) {
 			console.error('Đã xảy ra lỗi khi gửi dữ liệu:', error);
 			throw error;
 		}
-	};
-
+	}
+	
 	const handlesShowNotification = () => {
-		const show = window.confirm('Bạn có chắc chắn với lựa chọn này?');
+		const show = window.confirm("Bạn có chắc chắn với lựa chọn này?");
 		if (show) {
 			posthApi()
 				.then((res) => {
 					if (res) {
+						setGroupName("")
+						fetchApi().then((data)=>{
+							setData(data.data)
+						})
 						showToast('success', 'Đăng kí thành công!');
 					} else {
 						showToast('error', 'Đăng kí thất bại!');
@@ -97,11 +101,11 @@ function TopicGroup() {
 					showToast('error', 'Đã xảy ra lỗi khi gửi yêu cầu!');
 				});
 		}
-	};
+	}
 
 	return (
 		<div className={cx('wrapper')}>
-			<ToastContainer />
+			            <ToastContainer/>
 			<div className={cx('inner')}>
 				<div className={cx('name-page')}>Quản lý chung - Phân nhóm đề tài</div>
 
@@ -109,22 +113,16 @@ function TopicGroup() {
 					<div className={cx('frame-desc')}>
 						<div className={cx('text')}>Danh sách sinh viên đăng ký hướng nghiên cứu</div>
 						<div className={cx('frame-year')}>
-							<select className={cx('year')} id="year" name="year" onChange={handleYearChange}>
-								<option className={cx(selectedYear === '2020' && 'year-active')} value="2020">
-									2020-2021
-								</option>
-								<option className={cx(selectedYear === '2021' && 'year-active')} value="2021">
-									2021-2022
-								</option>
-								<option className={cx(selectedYear === '2022' && 'year-active')} value="2022">
-									2022-2023
-								</option>
-								<option className={cx(selectedYear === '2023' && 'year-active')} value="2023">
-									2023-2024
-								</option>
-							</select>
-						</div>
+
+						<select className={cx('year')} id="year" name="year" onChange={handleYearChange}>
+							<option className={cx(selectedYear === '2021' && 'year-active')} value="2021">2020-2021</option>
+                        	<option className={cx(selectedYear === '2022' && 'year-active')} value="2022">2021-2022</option>
+                        	<option className={cx(selectedYear === '2023' && 'year-active')} value="2023">2022-2023</option>
+                        	<option className={cx(selectedYear === '2024' && 'year-active')} value="2024">2023-2024</option>
+							
+						</select>
 					</div>
+				</div>
 
 					<table className={cx('table')}>
 						<thead className={cx('table-header')}>
@@ -134,19 +132,25 @@ function TopicGroup() {
 								<th className={cx('table-header-cell')}>Số Lượng Phân Công Tối Đa</th>
 							</tr>
 						</thead>
-						{data.map((newData) => (
-							<tbody key={newData.id}>
-								<tr className={cx('table-inner-row')}>
-									<td className={cx('table-inner-row-content')}>{newData.name}</td>
-									<td className={cx('table-inner-row-content')}>
-										{newData.students.map((dataStudents) => (
+						{
+							data.map(newData => 
+								<tbody key={newData.id}>
+							<tr className={cx('table-inner-row')}>
+								<td className={cx('table-inner-row-content')}>{newData.name}</td>
+								<td className={cx('table-inner-row-content')}>
+									{
+										newData.students.map(dataStudents=>
 											<div key={dataStudents.id}>{dataStudents.name}</div>
-										))}
-									</td>
-									<td className={cx('table-inner-row-content')}>{newData.quantity}</td>
-								</tr>
-							</tbody>
-						))}
+										)
+									}
+								</td>
+								<td className={cx('table-inner-row-content')}>{newData.quantity}</td>
+							</tr>
+							
+						</tbody>
+						)
+						}
+						
 					</table>
 					<div className={cx('frame-desc')}>
 						<div className={cx('text')}>Phân nhóm đề tài</div>
@@ -167,40 +171,32 @@ function TopicGroup() {
 													{...register('HNC')}
 													onChange={handleChangeSelected}
 												>
-													<option>-- Select --</option>
-													{data.map((newData) => (
-														<option key={newData.id} value={newData.id}>
-															{newData.name}
-														</option>
-													))}
+													<option >-- Select --</option>
+													{data.map(newData =>
+														<option key={newData.id} value={newData.id}>{newData.name}</option>)}
 												</select>
 											</div>
 										</div>
 									</div>
 									<div className={cx('item-wrap')}>
 										<div className={cx('item-title')}>Danh sách sinh viên</div>
-										{filterHNC.map((dataHNC) =>
-											dataHNC.students.map((dataStudents) => {
-												const handleAddStudentWithLogging = () => {
-													setIdStudent(dataStudents.id);
+										{filterHNC.map(dataHNC =>
+											dataHNC.students.map(dataStudents=>
+												{const handleAddStudentWithLogging = () => {
+													setIdStudent(dataStudents.id)
 													handleAddStudent(dataStudents.id);
 												};
-
+										
 												return (
 													<div key={dataStudents.id} className={cx('item-child')}>
 														<div className={cx('item-content')}>
 															<div className={cx('item-text')}>{dataStudents.name}</div>
 														</div>
-														<button
-															className={cx('btn-add')}
-															onClick={handleAddStudentWithLogging}
-														>
-															Thêm
-														</button>
+														<button className={cx('btn-add')} onClick={handleAddStudentWithLogging}>Thêm</button>
 													</div>
-												);
-											}),
-										)}
+												);}
+										))
+										}
 									</div>
 								</div>
 								<div className={cx('content-block')}>
@@ -215,48 +211,36 @@ function TopicGroup() {
 													value={groupName}
 													{...register('name')}
 													onChange={handleGroupNameChange}
+
 												/>
 											</div>
-											{showNotify && (
-												<div className={cx('notify')}>Tên nhóm không được để trống</div>
-											)}
+											{showNotify && <div className={cx('notify')}>Tên nhóm không được để trống</div>}
 										</div>
 									</div>
 
 									<div className={cx('item-wrap')}>
 										<div className={cx('item-title')}>Thành viên được chọn(*)</div>
-										<div className={cx('notify')}>Thành viên không được để trống</div>
+										{showNotify && <div className={cx('notify')}>Thành viên không được để trống</div>}
 
-										{filterHNC.map((newData) =>
-											newData.students
-												.filter((dataStudents) => arrStudents.includes(dataStudents.id))
-												.map((selectedStudent) => (
-													<div key={selectedStudent.id} className={cx('item-child')}>
-														<div className={cx('item-content')}>
-															<div className={cx('item-text')}>
-																{selectedStudent.name}
-															</div>
-														</div>
-														<input
-															type="hidden"
-															value={selectedStudent.id} // Giá trị của input
-														/>
-														<button
-															className={cx('btn-del')}
-															onClick={() => handleDeleteStudent(selectedStudent.id)}
-														>
-															Xóa
-														</button>
-													</div>
-												)),
-										)}
+										{filterHNC.map(newData =>
+                                            newData.students.filter(dataStudents =>arrStudents.includes(dataStudents.id)).map((selectedStudent) => (
+                                                <div key={selectedStudent.id} className={cx('item-child')}>
+                                                    <div className={cx('item-content')}>
+                                                        <div className={cx('item-text')}>{selectedStudent.name}</div>
+                                                    </div>
+													<input 
+														type="hidden" 
+														value={selectedStudent.id} // Giá trị của input
+													/>
+                                                    <button className={cx('btn-del')}  onClick={() => handleDeleteStudent(selectedStudent.id)}>Xóa</button>
+                                                </div>
+                                            ))
+                                        )}
 									</div>
 								</div>
 							</div>
 							<div className={cx('btn-container')}>
-								<button className={cx('btn-submit')} onClick={handleSubmit(handlesShowNotification)}>
-									Lưu
-								</button>
+								<button className={cx('btn-submit')} onClick={handleSubmit(handlesShowNotification)}>Lưu</button>
 							</div>
 						</div>
 					</div>
